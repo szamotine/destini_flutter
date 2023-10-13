5785e667e23b637cdcd916f523bb0cd3dd52553a
szamotine <77281802+szamotine@users.noreply.github.com>
2023-10-12 22:32:41 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/serguei/zamotine/destini/MainActivity.kt
@@ -0,0 +1,6 @@
+package serguei.zamotine.destini
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:destini/story_brain.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Destini());
+
+class Destini extends StatelessWidget {
+  const Destini({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  final StoryBrain storyBrain = StoryBrain();
+
+  StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+          minimum: const EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    widget.storyBrain.getStory().storyTitle,
+                    style: const TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    //Choice 1 made by user.
+                    setState(() {
+                      widget.storyBrain.nextStory(1);
+                    });
+                  },
+                  style: TextButton.styleFrom(backgroundColor: Colors.red),
+                  child: Text(
+                    widget.storyBrain.getChoice1(),
+                    style: const TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: widget.storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      setState(() {
+                        widget.storyBrain.nextStory(2);
+                      });
+                    },
+                    style: TextButton.styleFrom(backgroundColor: Colors.blue),
+                    child: Text(
+                      widget.storyBrain.getChoice2(),
+                      style: const TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
